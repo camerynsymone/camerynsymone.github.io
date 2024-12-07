7672a00402e5c1df949fe4dd154ece83b7666308
mstubbs415 <mstubbs415@gmail.com>
2024-12-07 05:32:42 -0800

    Update script.js

--- Wade-project.html/script.js
+++ Wade-project.html/script.js
@@ -2,14 +2,12 @@
 document.querySelectorAll('a').forEach(link => {
     // Add hover effect when the mouse hovers over text
     link.addEventListener('mouseenter', () => {
-        link.style.backgroundColor = '#FFD700'; // Gold background
-        link.style.color = '#004080'; // Deep blue color text
+        link.style.color = '#FFCC00'; // Gold text
         link.style.textDecoration = 'underline'; // Add underline
     });
 
     // Remove hover effect when the mouse leaves
     link.addEventListener('mouseleave', () => {
-        link.style.backgroundColor = 'transparent'; // Removes background
         link.style.color = ''; // Resets to default text color
         link.style.textDecoration = 'none'; // Removes the underline
     });
