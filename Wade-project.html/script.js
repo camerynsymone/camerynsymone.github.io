0d566b57a18771fb94051130c239a031ed1d32e5
mstubbs415 <mstubbs415@gmail.com>
2024-12-09 14:58:08 -0800

    Update script.js

--- Wade-project.html/script.js
+++ Wade-project.html/script.js
@@ -22,3 +22,22 @@ document.querySelectorAll('a').forEach(link => {
 $(function() {
     $("a").tooltip();
 });
+
+// Initialize jQuery-UI Accordion
+$(function() {
+    $("#weather-widget-container").accordion({
+        collapsible: true, // Allows all sections to collapse
+        heightStyle: "content" // Adjust height to fit the content
+    });
+});
+
+// Initialize the Weather Widget Script
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+})(document, 'script', 'weatherwidget-io-js');
